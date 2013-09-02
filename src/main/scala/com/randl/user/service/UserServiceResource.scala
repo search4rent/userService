8ce265b60fd4a50cbd174c5357be129e68791676
Pascal Fischer <pascal.fischer@holidaycheck.com>
2013-09-02 17:30:01 +0200

    forgot stuff

--- src/main/scala/com/randl/user/service/UserServiceResource.scala
+++ src/main/scala/com/randl/user/service/UserServiceResource.scala
@@ -5,6 +5,10 @@ import javax.ws.rs.{POST, Consumes, Produces, Path}
 import javax.ws.rs.core.{Response, Context}
 import javax.servlet.http.HttpServletRequest
 import javax.ws.rs.core.Response.Status
+import sun.security.provider.certpath.OCSPResponse.ResponseStatus
+import com.restfb.{DefaultFacebookClient, FacebookClient}
+import com.restfb.types.{User => FbUser}
+import com.codahale.jerkson.Json
 
 @Singleton
 @Path("/")
@@ -17,6 +21,18 @@ class UserServiceResource {
   @Path("")
   def create(user: User, @Context context: HttpServletRequest): Response = {
     Response.status(Status.OK).entity(Map("message" -> "service stub working, now implement me faster!")).build
+  }
+
+  @POST
+  @Path("userdata")
+  def userdata(token: String): Response = {
+    println("userdata called")
+    val client: FacebookClient = new DefaultFacebookClient(token)
+    println("got client")
+    val fbUser: FbUser = client.fetchObject("me", classOf[FbUser])
+    println("user:\n " + Json.generate(fbUser))
+
+    Response.status(Status.OK).entity(Map("name" -> fbUser.getName)).build
   }
 
 }
